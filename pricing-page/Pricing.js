42e44ff670edc7c22c12b4aa5ae702480f0f3e09
thgstarburst <kng024@uottawa.ca>
2020-10-25 23:19:17 -0400

    Final changes

--- pricing-page/Pricing.js
+++ pricing-page/Pricing.js
@@ -1,4 +1,12 @@
-function date(){
-    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    var Day = document.getElementById("time").innerHTML=.
+function date() {
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var Day = document.getElementsByClassName("Weekday").innerHTML = days[d.getDay()];
+
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var Month = document.getElementsByClassName("Month").innerHTML = months[d.getMonth() + 1];
+
+    var DayNumber = document.getElementsByClassName("Day").innerHTML = months[d.getMonth() + 1];
+
+    var Year = document.getElementsByClassName("Year").innerHTML =d.getFillYear();
+    /* Future code for putting today's date */
 }
