617a3b63cde02d8d26543d96743efc25040a8b8e
Joy Ye <zijunye001@gmail.com>
2020-11-10 18:48:39 -0500

    feature section

--- home.js
+++ home.js
@@ -1,19 +1,20 @@
-var page = 1,
-    moving = false;
-var animationIteration = "animationiteration webkitAnimationIteration mozAnimationIteration oAnimationIteration oanimationiteration",
-	transitionEnd      = "transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd";
-$(".load-more").on("click", function() {
-  if ( moving == false ) {
-    moving = true;
-    $(".load-more").addClass("active");
-    setTimeout(function() {
-      $(".load-more").one(animationIteration, function() {
-        $(".load-more").removeClass("active");
-        $(".load-more").one(transitionEnd, function() {
-          page++;
-          moving = false;
-        });
-      });
-    }, 2000);
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
-});
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
